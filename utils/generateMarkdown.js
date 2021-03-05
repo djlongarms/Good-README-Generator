4b13d73072150cb51eb8b457da94b4fae10ccac4
Nick Palmer <ylnickpalmer@gmail.com>
2021-03-05 13:04:51 -0800

    added table of contents for generated README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,6 +7,14 @@ function generateMarkdown(data) {
 ${data.description}
 
 ## Table of Contents
+[Title](#title)
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Tests](#tests)
+[Questions](#questions)
 
 ## Installation
 
